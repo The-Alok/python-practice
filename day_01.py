5b09ab373cb82a09b5c8f74220e7fe9fd80b48f9
Alok Awasthi <65930672+The-Alok@users.noreply.github.com>
2026-06-17 01:11:43 +0530

    Day 01 Python practice

---
+++ day_01.py
@@ -0,0 +1,15 @@
+print("Hello ! World")
+age = 30
+name = "Alok Awasthi"
+city = "Bahraich"
+print(f"\nMy name is {name}. I am {age} years old and live in {city}")
+a = int(input("\nEnter first number : "))
+b = int(input("Enter second number : "))
+print(f"\nSum = {a + b}")
+print(f"Difference = {a - b}")
+print(f"Product = {a * b}")
+print(f"Division = {a / b}")
+name = input("\nEnter you name: ")
+age = input("Enter your age: ")
+profession = input("Enter you profession: ")
+print(f"\n ------- Bio -------\n Name: {name} \n Age: {age} \n Profession:{profession} \n -------------------")
